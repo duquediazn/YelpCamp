d3e1d6e821c24fb1892e53280fbfea3d2a0e9af7
duquediazn <duquediazn@gmail.com>
2025-08-14 14:18:52 +0100

    refactor: use router.route to group together similar routes

--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,21 @@ const users = require("../controllers/users")
 const passport = require("passport");
 const { storeReturnTo } = require('../middleware'); // import the storeReturnTo function
 
-// GET route to display the registration form
-router.get("/register", users.renderRegister);
+router.route("/register")
+  .get(users.renderRegister)
+  .post(users.createUser);
 
-// POST route to handle the registration form submission
-router.post("/register", users.createUser);
+router.route("/login")
+  .get(users.renderLogin)
+  .post(
+    storeReturnTo, // Use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    passport.authenticate("local", { // Real login
+      failureFlash: true,            // Show flash message on failed login
+      failureRedirect: "/login"      // Redirect back to login page if authentication fails
+    }),
+    users.login
+  );
 
-// GET route to display the login form
-router.get("/login", users.renderLogin);
-
-// POST route to process login form using Passport's local strategy
-router.post(
-  "/login",
-  storeReturnTo, // Use the storeReturnTo middleware to save the returnTo value from session to res.locals
-  passport.authenticate("local", { // Real login
-    failureFlash: true,            // Show flash message on failed login
-    failureRedirect: "/login"      // Redirect back to login page if authentication fails
-  }),
-  users.login
-);
-
-// GET route to log the user out
 router.get("/logout", users.logout);
 
 module.exports = router;
