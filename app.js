690dc627760155365a067824a28ffa5e8ff865e6
duquediazn <duquediazn@gmail.com>
2025-08-26 18:51:53 +0100

    feat(security): add helmet with CSP configuration
    - install helmet
    
    - configure Content Security Policy (CSP) with allowed script, style, connect, image, and font sources
    
    - enhance security by restricting external resource loading

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const passport = require("passport"); // Import Passport authentication middlewa
 const LocalStrategy = require("passport-local"); // Import Passport strategy for authenticating with a username and password.
 const User = require("./models/user"); // Import the User model
 const mongoSanitize = require('express-mongo-sanitize'); // To prevent mongo injection
+const helmet = require("helmet"); // Help secure Express apps by setting HTTP response headers. https://www.npmjs.com/package/helmet
 
 
 // Route files
@@ -67,6 +68,48 @@ const sessionConfig = {
 app.use(session(sessionConfig)); // This shoud be used BEFORE using passport.session()
 
 app.use(flash()); // We’re using the connect-flash package to add flash message support.
+app.use(helmet());
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.maptiler.com/",
+];
+const connectSrcUrls = [
+  "https://api.maptiler.com/",
+];
+
+const fontSrcUrls = [];
+
+// Helmet CSP configuration
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: [],
+    connectSrc: ["'self'", ...connectSrcUrls],
+    scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+    styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+    workerSrc: ["'self'", "blob:"],
+    imgSrc: [
+      "'self'",
+      "blob:",
+      "data:",
+      `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+      "https://images.unsplash.com",
+      "https://api.maptiler.com/"
+    ],
+    fontSrc: ["'self'", ...fontSrcUrls]
+  }
+}))
 
 // Passport configuration
 app.use(passport.initialize()); // Initializes Passport for authentication handling
